0d24f4479375b62b1ce67a64dbc97715a1b81cdd
Niko.Knez <info@design-nk.de>
2023-10-16 15:34:47 +0200

    update

--- wp-job-listing-integration/wp-job-listing-integration.php
+++ wp-job-listing-integration/wp-job-listing-integration.php
@@ -26,6 +26,7 @@ function wpjli_enqueue_admin_styles()
   wp_enqueue_style('wpjli-admin-style', plugin_dir_url(__FILE__) . 'assets/css/admin-style.css');
 }
 
+// Weitere Funktionen und Handler können hier hinzugefügt werden...
 function wpjli_upload_form_shortcode()
 {
   // Überprüfen, ob der Benutzer eingeloggt ist
@@ -33,19 +34,59 @@ function wpjli_upload_form_shortcode()
     return 'Bitte melden Sie sich an, um eine Stellenanzeige hochzuladen.';
   }
 
+  $output = ''; // Variable für die Ausgabe
+
+  // Wenn das Formular abgesendet wurde
+  if (isset($_POST['submit']) && isset($_FILES['jobListing']) && wp_verify_nonce($_POST['_wpnonce'], 'wpjli_upload_nonce')) {
+    $uploadedFile = $_FILES['jobListing'];
+
+    // Überprüfen, ob es sich um eine .zip-Datei handelt
+    if ($uploadedFile['type'] == 'application/zip') {
+      $zip = new ZipArchive;
+      $res = $zip->open($uploadedFile['tmp_name']);
+      if ($res === TRUE) {
+        // Bestimmen Sie den Pfad zum Upload-Verzeichnis
+        $upload_dir = wp_upload_dir();
+        $base_upload_path = $upload_dir['basedir'];
+
+        // Fügen Sie den Unterordner 'HTMLupload' hinzu
+        $extractPath = $base_upload_path . '/HTMLupload';
+
+        // Stellen Sie sicher, dass das Verzeichnis existiert (wenn nicht, erstellen Sie es)
+        if (!file_exists($extractPath)) {
+          mkdir($extractPath, 0755, true);
+        }
+
+        // Entpacken Sie das Archiv in das festgelegte Verzeichnis
+        $zip->extractTo($extractPath);
+        $zip->close();
+
+        // Überprüfen Sie, ob die erforderlichen Dateien vorhanden sind
+        if (file_exists($extractPath . '/index.html') && file_exists($extractPath . '/style.css')) {
+          // Hier können Sie den Inhalt von index.html und style.css verarbeiten und in einen WordPress-Beitrag konvertieren
+          $output .= "Dateien erfolgreich hochgeladen und verarbeitet!";
+        } else {
+          $output .= "Die erforderlichen Dateien fehlen im Archiv.";
+        }
+      } else {
+        $output .= "Fehler beim Öffnen des .zip-Archivs.";
+      }
+    } else {
+      $output .= "Bitte laden Sie nur .zip-Dateien hoch.";
+    }
+  }
+
   ob_start(); // Starte die Ausgabepufferung
 
   // Ihr Formularcode hier
-?>
-  <form action="<?php echo esc_url(admin_url('admin-post.php')); ?>" method="post" enctype="multipart/form-data">
-    <input type="hidden" name="action" value="wpjli_handle_upload">
-    <?php wp_nonce_field('wpjli_upload_nonce', 'wpjli_nonce'); ?>
-    <label for="jobListing">Wählen Sie die Dateien zum Hochladen aus:</label>
-    <input type="file" name="jobListing" id="jobListing">
-    <input type="submit" value="Hochladen">
-  </form>
-<?php
+  $output .= '
+  <form action="" method="post" enctype="multipart/form-data">
+      ' . wp_nonce_field('wpjli_upload_nonce', '_wpnonce', true, false) . '
+      <label for="jobListing">Wählen Sie die Dateien zum Hochladen aus:</label>
+      <input type="file" name="jobListing" id="jobListing" accept=".zip">
+      <input type="submit" name="submit" value="Hochladen">
+  </form>';
 
-  return ob_get_clean(); // Gib den gepufferten Inhalt zurück
+  return $output; // Gib den generierten Inhalt zurück
 }
 add_shortcode('wpjli_upload_form', 'wpjli_upload_form_shortcode');
